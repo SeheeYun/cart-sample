fc623d817c66718026cfc7d50105f088b32375ba
sehee yun <seheeyun29@gmail.com>
2022-02-17 14:17:04 +0900

    ➕ fontawesome 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  return <div className="App">asdsa</div>;
+  return (
+    <div className="App">
+      <header>
+        <button>
+          <FontAwesomeIcon icon={faXmark} />
+        </button>
+        <div>윤세희</div>
+        <button>
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </header>
+      <main>main</main>
+      <footer>footer</footer>
+    </div>
+  );
 }
 
 export default App;
